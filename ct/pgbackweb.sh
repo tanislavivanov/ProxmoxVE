d51f55e886b3db7cac8bb1f2f03887cae670753b
Tanislav <54242123+tanislavivanov@users.noreply.github.com>
2026-08-04 20:58:29 +0300

    test

--- ct/pgbackweb.sh
+++ ct/pgbackweb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/tanislavivanov/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: Tanislav Ivanov (tanislavivanov)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
